1239aef36e06e6b53794a0bb479d804c5f7be909
seongminnpark <seongmi1@andrew.cmu.edu>
2016-10-18 01:14:08 -0400

    Add root checker functionality.

--- app/src/main/java/com/example/seongmin/terminal/MainActivity.java
+++ app/src/main/java/com/example/seongmin/terminal/MainActivity.java
@@ -6,8 +6,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import eu.chainfire.libsuperuser.Shell;
+
 public class MainActivity extends AppCompatActivity {
     private TextView console_output;
+    private TextView root_output;
     private Button ls_button;
 
     @Override
@@ -17,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Initialize View components.
         console_output = (TextView) findViewById(R.id.console_output);
+        root_output = (TextView) findViewById(R.id.root_output);
         ls_button = (Button) findViewById(R.id.ls_button);
 
         // Set onclick listeners.
@@ -26,5 +30,17 @@ public class MainActivity extends AppCompatActivity {
                 console_output.setText("Clicked");
             }
         });
+
+        display_root_status();
+    }
+
+    private void display_root_status() {
+        String rootStatus;
+        if (Shell.SU.available()) {
+            rootStatus = "Root available.";
+        } else {
+            rootStatus = "Root unavailable.";
+        }
+        root_output.setText(rootStatus);
     }
 }
